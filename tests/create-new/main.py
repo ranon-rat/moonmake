743c7482b55f147937998ef127f682ffc9893b4f
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-03-25 07:17:33 -0600

    base

--- tests/create-new/main.py
+++ tests/create-new/main.py
@@ -5,41 +5,37 @@ import argparse
 
 BASE_MOONFILE="""#https://github.com/ranon-rat/moonmake
 import moonmake as mmake
-import platform
 from os.path import join
-
 dir_path =mmake.get_dir(__file__)
 # Lista los archivos en el directorio
 if __name__=="__main__":
-    moonmake=".moonmake"
-    
-    CPP_VERSION="14"
-    INCLUDE=f"{mmake.join_with_flag([join(".",moonmake,"dependencies","headers")],"-I")} -I{join(dir_path,"src","include")}"
-    mmake.discover()
-    FLAGS=f"-Wall -Wextra -std=c++${CPP_VERSION}"
-    LINK=f"{mmake.join_with_flag([join(".",moonmake,"dependencies","lib")],"-L")} -L{join(".",dir_path,moonmake,"lib")}"
+    moonmake_dir=".moonmake"
+    CPP_VERSION="17"
+    INCLUDE=f"{mmake.join_with_flag([join(".",moonmake_dir,"dependencies","headers")],"-I")} -I{join(dir_path,"src","include")}"
+    FLAGS=f"-Wall -Wextra -std=c++{CPP_VERSION}"
+    LINK=f"{mmake.join_with_flag([join(moonmake_dir,"dependencies","lib")],"-L")} -L{join(dir_path,moonmake_dir,"lib")}"
 
     extension=mmake.get_extension()
     main=mmake.Builder()
-    static_a_files=mmake.Builder(join(".",moonmake,"dependencies","lib"),".a")
-    dir_a_files=mmake.change_extension(static_a_files,join(".",moonmake,"dependencies","lib","a"))
+    static_a_files=mmake.discover(join(".",moonmake_dir,"dependencies","lib"),".a")
     STATIC_LIBRARY=" ".join([f"-l{a.replace("lib","").replace(".a","")}" for a in static_a_files])
 
     target_files=list([f for f in  mmake.discover(join(dir_path,"src","target"),".cpp")])
-    target_obj=mmake.change_extension(target_files,join(dir_path,moonmake,"obj","target"),old=".cpp",new=".o")
-    target_bin=mmake.change_extension(target_files,join(dir_path,moonmake,"bin"),old=".cpp",new=extension)
+    target_obj=mmake.change_extension(target_files,join(dir_path,moonmake_dir,"obj","target"),old=".cpp",new=".o")
+    target_bin=mmake.change_extension(target_files,join(dir_path,moonmake_dir,"bin"),old=".cpp",new=extension)
 
     lib_files=list([f for f in  mmake.discover(join(dir_path,"src","lib"),".cpp")])
-    lib_obj=mmake.change_extension(lib_files,join(dir_path,"obj","lib"),old=".cpp",new=".o")
-    lib_static=join(dir_path,moonmake,"lib","libsrc.a")
+    lib_obj=mmake.change_extension(lib_files,join(dir_path,moonmake_dir,"obj","lib"),old=".cpp",new=".o")
+    lib_static=join(dir_path,moonmake_dir,"lib","libmsrc.a")
     #so we generate the binaries
-    main.watch(target_bin,target_obj,f"g++ $< -o $@ {FLAGS} {LINK} {STATIC_LIBRARY} -lsrc",extra_dependencies=[lib_static,*dir_a_files])
+    print(target_bin)
+    main.watch(list(map(lambda r:r,target_bin)),list(map(lambda r:r,target_obj)),f"g++ $< -o $@ {FLAGS} {LINK} {STATIC_LIBRARY} -lmsrc",extra_dependencies=[lib_static,*static_a_files])
     #object files for the target_files :D
-    main.watch(target_obj,target_files,f"g++ -c $< -o $@ {FLAGS} {INCLUDE} ")
+    main.watch(target_obj,list(map(lambda r:join(".","src","target",r),target_files)),f"g++ -c $< -o $@ {FLAGS} {INCLUDE} ")
     #we create a library for later linking it with our target binaries
     main.watch([lib_static],lib_obj,"ar rcs $@ $^")
     #we generate the object files of thebinaries
-    main.watch(lib_obj,lib_files,f"g++ -c $< -o $@ {FLAGS} {INCLUDE}")
+    main.watch(lib_obj,list(map(lambda r:join(".","src","lib",r),lib_files)),f"g++ -c $< -o $@ {FLAGS} {INCLUDE}")
 
     main.compile_all()
 
