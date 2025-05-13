c0d41c187cdf59d92a8f143362c5abaa0521ba81
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-05-13 01:47:10 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="moonmake",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
